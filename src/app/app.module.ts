49ece34e63eee625ed405eb7e6e4d0aa519ac9a9
krasotun <servrostov@gmail.com>
2022-04-14 09:42:21 +0300

    fix Исправил ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
